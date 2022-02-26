b6d4a08c454c1dbfcbdd20e88821c6a92da1e766
JadineM <99989910+JadineM@users.noreply.github.com>
2022-02-26 16:46:23 +0200

    Add files via upload
    Add create database script

---
+++ create_college_db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE college;
+USE college;
+CREATE TABLE student(firstname varchar(30), 
+					 surname varchar(30), 
+					 algebra_mark int, 
+					 calculus_mark int, 
+					 programming_mark int, 
+					 databases_mark int);
+
+
